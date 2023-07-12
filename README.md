48deadcf7703be7fc789a5945469864703c7a684
youneshenniwrites <youneshenniwrites@gmail.com>
2023-07-12 21:29:25 +0100

    vscode extension to replace filname extensions

---
+++ README.md
@@ -0,0 +1,40 @@
+# Rename Extensions
+
+This is a Visual Studio Code extension that allows you to easily rename file extensions within a specified directory.
+
+## Features
+
+- Renames file extensions within a directory and its subdirectories.
+- Supports renaming from one extension to another.
+- Provides visual feedback for each renamed file.
+
+## Requirements
+
+- Visual Studio Code
+
+## Usage
+
+1. Launch Visual Studio Code.
+2. Open the directory containing the files you want to rename.
+3. Press `Ctrl+Shift+P` (or `Cmd+Shift+P` on macOS) to open the Command Palette.
+4. Type "Rename Extensions" and select the "Rename Extensions" command.
+5. In the input boxes, enter the directory path, target extension, and replacement extension.
+6. Press `Enter` to start the renaming process.
+7. The extension will traverse the specified directory and rename the file extensions accordingly.
+8. Information messages will be displayed for each renamed file.
+
+## Extension Settings
+
+This extension does not provide any customizable settings.
+
+## Known Issues
+
+- No known issues.
+
+## Contributing
+
+Contributions are welcome! If you have any bug reports, suggestions, or improvements, please open an issue or submit a pull request on the [GitHub repository](https://github.com/your-username/rename-extensions).
+
+## License
+
+This extension is licensed under the [MIT License](LICENSE).
---
+++ src/extension.ts
@@ -0,0 +1,91 @@
+import * as vscode from "vscode";
+import * as path from "path";
+import * as fs from "fs";
+
+function renameFileExtensions(
+  directory: string,
+  targetExtension: string,
+  replacementExtension: string
+) {
+  fs.readdir(directory, (err, files) => {
+    if (err) {
+      vscode.window.showErrorMessage(`Failed to read directory: ${err}`);
+      return;
+    }
+
+    files.forEach((filename) => {
+      const filePath = path.join(directory, filename);
+
+      fs.stat(filePath, (err, stats) => {
+        if (err) {
+          vscode.window.showErrorMessage(`Failed to get file stats: ${err}`);
+          return;
+        }
+
+        if (stats.isFile() && filename.endsWith(targetExtension)) {
+          const newFilename = filename.replace(
+            targetExtension,
+            replacementExtension
+          );
+          const newFilePath = path.join(directory, newFilename);
+
+          fs.rename(filePath, newFilePath, (err) => {
+            if (err) {
+              vscode.window.showErrorMessage(`Failed to rename file: ${err}`);
+              return;
+            }
+
+            vscode.window.showInformationMessage(
+              `Renamed: ${filename} -> ${newFilename}`
+            );
+          });
+        }
+      });
+    });
+  });
+}
+
+export function activate(context: vscode.ExtensionContext) {
+  let disposable = vscode.commands.registerCommand(
+    "extension.renameExtensions",
+    () => {
+      vscode.window
+        .showInputBox({ prompt: "Enter the directory path:" })
+        .then((directory) => {
+          if (!directory) {
+            return;
+          }
+
+          vscode.window
+            .showInputBox({
+              prompt: "Enter the target extension (e.g., .jsx):",
+            })
+            .then((targetExtension) => {
+              if (!targetExtension) {
+                return;
+              }
+
+              vscode.window
+                .showInputBox({
+                  prompt: "Enter the replacement extension (e.g., .tsx):",
+                })
+                .then((replacementExtension) => {
+                  if (!replacementExtension) {
+                    return;
+                  }
+
+                  renameFileExtensions(
+                    directory,
+                    targetExtension,
+                    replacementExtension
+                  );
+                });
+            });
+        });
+    }
+  );
+
+  context.subscriptions.push(disposable);
+}
+
+export function deactivate() {}
